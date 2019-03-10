af0dcd87ca0cc79f14f90286bd7ca624bd89c545
Vygantas <vygantasskirius@gmail.com>
2019-03-10 15:49:25 +0200

    cors fix

--- src/server.js
+++ src/server.js
@@ -3,15 +3,17 @@ import graphqlHTTP from "express-graphql";
 import { printSchema } from "graphql";
 import { writeFile } from 'fs';
 import Schema from './data/Schema';
+import cors from 'cors';
 
 const app = express();
+app.use(cors());
 app.use('/graphql', graphqlHTTP({
   schema: Schema,
   graphiql: true,
 }));
 app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
 
-writeFile('./graphql/schema.graphql', printSchema(Schema), err => {
+/*writeFile('./graphql/schema.graphql', printSchema(Schema), err => {
   if(err) throw err;
   console.log("GRAPHQL schema created");
-})
+})*/
